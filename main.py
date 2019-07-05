eb306f9d2d2a07e1eba64080d05208861d8020f7
derekyin <derek.yin@uwaterloo.ca>
2019-07-05 14:15:34 -0400

    mean sd and var for individual stocks

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from datetime import datetime
 import sys
 from yahoo_finance_api2 import share
 from yahoo_finance_api2.exceptions import YahooFinanceError
-
+import numpy as np
+import stock
 
 ticker_a = input("Enter stock ticker A\n")
 ticker_b = input("Enter stock ticker B\n")
@@ -25,10 +26,25 @@ except YahooFinanceError as e:
     print(e.message)
     sys.exit(1)
 
-print("----{}----".format(ticker_a))
+stock_A = stock.Stock(ticker_a)
+stock_B = stock.Stock(ticker_b)
+
 for i in range (0, len(a_data["timestamp"])):
-    print(datetime.fromtimestamp(int(str(a_data["timestamp"][i])[:-3])), a_data["close"][i])
+    stock_A.date.append(datetime.fromtimestamp(int(str(a_data["timestamp"][i])[:-3])))
+    stock_A.closing_price.append(a_data["close"][i])
+
+for i in range (0, len(b_data["timestamp"])):
+    stock_B.date.append(datetime.fromtimestamp(int(str(b_data["timestamp"][i])[:-3])))
+    stock_B.closing_price.append(b_data["close"][i])
+
+print("----{}----".format(stock_A.ticker))
+a_mean = np.mean(stock_A.closing_price)
+print("mean of {}: ".format(stock_A.ticker), np.mean(stock_A.closing_price)) 
+print("s.d of {}: ".format(stock_A.ticker), np.std(stock_A.closing_price)) 
+print("variance of {}: ".format(stock_A.ticker), np.var(stock_A.closing_price))
 
 print("----{}----".format(ticker_b))
-for i in range (0, len(b_data["timestamp"])):
-    print(datetime.fromtimestamp(int(str(b_data["timestamp"][i])[:-3])), b_data["close"][i])
+b_mean = np.mean(stock_B.closing_price)
+print("mean of {}: ".format(stock_B.ticker), np.mean(stock_B.closing_price)) 
+print("s.d of {}: ".format(stock_B.ticker), np.std(stock_B.closing_price)) 
+print("variance of {}: ".format(stock_B.ticker), np.var(stock_B.closing_price))
---
+++ stock.py
@@ -0,0 +1,5 @@
+class Stock:
+    def __init__(self, ticker, date = [], closing_price = []):
+        self.ticker = ticker
+        self.date = date
+        self.closing_price = closing_price
