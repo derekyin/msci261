ba7f05cb813ab8ad22bcce365eb7c378abdadd22
Jiahao Zhang <zhang.jiahao@hotmail.com>
2019-07-20 11:45:17 -0400

    Refactoring. Delegating more to objects.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from datetime import datetime
-import sys
-from yahoo_finance_api2 import share
-from yahoo_finance_api2.exceptions import YahooFinanceError
 import numpy as np
 import stock
 
@@ -11,34 +7,8 @@ def main(ticker_a=None, ticker_b=None):
     if not ticker_b:
         ticker_b = input("Enter stock ticker B\n")
 
-    stock_a = share.Share(ticker_a)
-    stock_b = share.Share(ticker_b)
-
-    a_data = None
-    b_data = None
-
-    try:
-        a_data = stock_a.get_historical(share.PERIOD_TYPE_YEAR,1, share.FREQUENCY_TYPE_DAY, 1)
-    except YahooFinanceError as e:
-        print(e.message)
-        sys.exit(1)
-
-    try:
-        b_data = stock_b.get_historical(share.PERIOD_TYPE_YEAR,1, share.FREQUENCY_TYPE_DAY, 1)
-    except YahooFinanceError as e:
-        print(e.message)
-        sys.exit(1)
-
     stock_A = stock.Stock(ticker_a)
     stock_B = stock.Stock(ticker_b)
-
-    for i in range (0, len(a_data["timestamp"])):
-        stock_A.date.append(datetime.fromtimestamp(int(a_data["timestamp"][i])/1000))
-        stock_A.closing_price.append(a_data["close"][i])
-
-    for i in range (0, len(b_data["timestamp"])):
-        stock_B.date.append(datetime.fromtimestamp(int(b_data["timestamp"][i])/1000))
-        stock_B.closing_price.append(b_data["close"][i])
 
     print("----{}----".format(stock_A.ticker))
     print("mean return of {}: ".format(stock_A.ticker), stock_A.get_average()) 
