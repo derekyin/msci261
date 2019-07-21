6adf6cb32241308a670e1a7447165ffdffb846bd
danielzhang130 <37479705+danielzhang130@users.noreply.github.com>
2019-07-21 09:43:22 -0400

    Flask docker (#3)
    * Added flask endpoint.
    
    * Refactoring. Delegating more to objects.
    
    * Added Dockerfile and production server. UNTESTED.

--- stock.py
+++ stock.py
@@ -1,25 +1,47 @@
+from datetime import datetime
+from yahoo_finance_api2 import share
+from yahoo_finance_api2.exceptions import YahooFinanceError
 import numpy as np
 class Stock:
     def __init__(self, ticker):
         self.ticker = ticker
         self.date = []
         self.closing_price = []
+        _data = None
+
+        _share = share.Share(self.ticker)
+        try:
+            _data = _share.get_historical(
+                    share.PERIOD_TYPE_YEAR, 1, share.FREQUENCY_TYPE_DAY, 1)
+        except YahooFinanceError as e:
+            print(e.message)
+            sys.exit(1)
+
+        for i in range (0, len(_data["timestamp"])):
+            self.date.append(
+                    datetime.fromtimestamp(int(_data["timestamp"][i])/1000))
+            self.closing_price.append(_data["close"][i])
+
+        self.returns = []
+        for i in range(1, len(self.closing_price)):
+            self.returns.append(
+                    self.closing_price[i]/self.closing_price[i-1] - 1)
+        self.mean = np.mean(self.get_returns())
+        self.average = np.mean(self.get_returns())
+        self.stddev = np.std(self.get_returns())
+        self.var = np.var(self.get_returns())
 
     def get_returns(self):
-        returns = []
-        for i in range(1, len(self.closing_price)):
-            returns.append(self.closing_price[i]/self.closing_price[i-1] - 1)
-
-        return returns
+        return self.returns
     
     def get_average(self):
-        return np.mean(self.get_returns())
+        return self.average
 
     def get_var(self):
-        return np.var(self.get_returns())
+        return self.var
 
     def get_stddev(self):
-        return np.std(self.get_returns())
+        return self.stddev
 
 class Portfolio:
     def __init__(self, stddev, var, avg_return, proportion):
