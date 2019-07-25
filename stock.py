c1469ef64e42e536077ff0332d645d656667b303
danielzhang130 <37479705+danielzhang130@users.noreply.github.com>
2019-07-25 09:16:12 -0400

    Flask docker (#10)
    * Changes:
    
    - Made Portfolio class more self sufficient.
    - Added MVP optimizer instead of brute-force.
    - Added classmethods for combining stocks and representing risk free as a stock

--- stock.py
+++ stock.py
@@ -1,7 +1,15 @@
 from datetime import datetime
+from typing import List
+from copy import copy
+import logging
 from yahoo_finance_api2 import share
 from yahoo_finance_api2.exceptions import YahooFinanceError
 import numpy as np
+
+def stddev(x, cov_matrix):
+    return np.sqrt(x.dot(cov_matrix).dot(x.T))
+
+
 class Stock:
     def __init__(self, ticker):
         self.ticker = ticker
@@ -14,7 +22,7 @@ class Stock:
             _data = _share.get_historical(
                     share.PERIOD_TYPE_YEAR, 1, share.FREQUENCY_TYPE_DAY, 1)
         except YahooFinanceError as e:
-            print(e.message)
+            logging.getLogger(__name__).critical(e.message)
             sys.exit(1)
 
         for i in range (0, len(_data["timestamp"])):
@@ -26,7 +34,7 @@ class Stock:
         for i in range(1, len(self.closing_price)):
             self.returns.append(
                     self.closing_price[i]/self.closing_price[i-1] - 1)
-        self.mean = np.mean(self.get_returns())
+
         self.average = np.mean(self.get_returns())
         self.stddev = np.std(self.get_returns())
         self.var = np.var(self.get_returns())
@@ -46,12 +54,46 @@ class Stock:
     def get_stddev(self):
         return self.stddev
 
+    @classmethod
+    def combine(cls, proportions : np.ndarray, stocks : List):
+        combined = copy(stocks[0])
+        combined.ticker = "portfolio " + " ".join([stock.ticker for stock in stocks])
+        combined.closing_price = []
+        combined.returns = []
+
+        for i in range(len(stocks[0].closing_price)):
+            combined.closing_price.append(proportions.dot([stock.closing_price[i] for stock in stocks]))
+
+        for i in range(len(stocks[0].returns)):
+            combined.returns.append(proportions.dot([stock.returns[i] for stock in stocks]))
+
+        combined.average = np.mean(combined.get_returns())
+
+        tmp = Portfolio(proportions, stocks)
+        combined.stddev = tmp.stddev
+        combined.var = tmp.var
+        return combined
+
+
+    @classmethod
+    def risk_free(cls, rate, other):
+        rf = copy(other)
+        rf.ticker = "risk free " + str(rate)
+        rf.closing_price = []
+        rf.average = (1 + rate)**(1/len(other.returns)) - 1
+        rf.returns = [rf.average for i in other.returns]
+        rf.stddev = 0
+        rf.var = 0
+        return rf
+
 class Portfolio:
-    def __init__(self, stddev, var, avg_return, proportion):
-        self.stddev = stddev
-        self.var = var
-        self.avg_return = avg_return
-        self.proportion = proportion
+    def __init__(self, proportions : np.ndarray, stocks : List[Stock]):
+        self.proportions = proportions
+        self.stocks = stocks
+        self.cov_matrix = np.cov([stock.get_returns() for stock in stocks])
+        self.stddev = stddev(self.proportions, self.cov_matrix)
+        self.var = self.stddev**2
+        self.avg_return = proportions.dot([stock.get_annual_return() for stock in stocks])
 
     def __lt__(self, other):
         return self.var < other.var
