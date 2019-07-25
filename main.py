9fc16675c233669e94e66ef93e26793127d586b5
Jiahao Zhang <zhang.jiahao@hotmail.com>
2019-07-25 18:15:33 -0400

    Merge files. Deployed two functions.

--- main.py
+++ main.py
@@ -1,11 +1,126 @@
+from copy import copy
+from datetime import datetime
+from flask import request, jsonify
 from random import random
 from scipy import optimize
-from stock import stddev
 from typing import List
+from yahoo_finance_api2 import share
 from yahoo_finance_api2.exceptions import YahooFinanceError
 import logging
 import numpy as np
-import stock
+import sys
+
+
+def get(request):
+    request_json = request.get_json()
+
+    stock_a = request_json['a']
+    stock_b = request_json['b']
+
+    if stock_a and stock_b:
+        return jsonify(main(stock_a.strip(), stock_b.strip()))
+    return jsonify({})
+
+
+def stddev(x, cov_matrix):
+    return np.sqrt(x.dot(cov_matrix).dot(x.T))
+
+
+class Stock:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.date = []
+        self.closing_price = []
+        _data = None
+
+        _share = share.Share(self.ticker)
+        _data = _share.get_historical(
+            share.PERIOD_TYPE_YEAR, 1, share.FREQUENCY_TYPE_DAY, 1)
+        if not _data:
+            raise ValueError("No data on stock {}".format(ticker))
+
+        for i in range(0, len(_data["timestamp"])):
+            self.date.append(
+                datetime.fromtimestamp(int(_data["timestamp"][i])/1000))
+            self.closing_price.append(_data["close"][i])
+
+        self.returns = []
+        for i in range(1, len(self.closing_price)):
+            if not self.closing_price[i] or not self.closing_price[i-1]:
+                self.returns.append(0)
+            else:
+                self.returns.append(
+                    self.closing_price[i]/self.closing_price[i-1] - 1)
+
+        self.average = np.mean(self.get_returns())
+        self.stddev = np.std(self.get_returns())
+        self.var = np.var(self.get_returns())
+
+    def get_returns(self):
+        return self.returns
+
+    def get_annual_return(self):
+        return (1 + self.get_average()) ** len(self.get_returns()) - 1
+
+    def get_average(self):
+        return self.average
+
+    def get_var(self):
+        return self.var
+
+    def get_stddev(self):
+        return self.stddev
+
+    @classmethod
+    def combine(cls, proportions: np.ndarray, stocks: List):
+        combined = copy(stocks[0])
+        combined.ticker = "portfolio " + \
+            " ".join([stock.ticker for stock in stocks])
+        combined.closing_price = []
+        combined.returns = []
+
+        for i in range(len(stocks[0].closing_price)):
+            combined.closing_price.append(proportions.dot(
+                [stock.closing_price[i] for stock in stocks]))
+
+        for i in range(len(stocks[0].returns)):
+            combined.returns.append(proportions.dot(
+                [stock.returns[i] for stock in stocks]))
+
+        combined.average = np.mean(combined.get_returns())
+
+        tmp = Portfolio(proportions, stocks)
+        combined.stddev = tmp.stddev
+        combined.var = tmp.var
+        return combined
+
+    @classmethod
+    def risk_free(cls, rate, other):
+        rf = copy(other)
+        rf.ticker = "risk free " + str(rate)
+        rf.closing_price = []
+        rf.average = (1 + rate)**(1/len(other.returns)) - 1
+        rf.returns = [rf.average for i in other.returns]
+        rf.stddev = 0
+        rf.var = 0
+        return rf
+
+
+class Portfolio:
+    def __init__(self, proportions: np.ndarray, stocks: List[Stock]):
+        self.proportions = proportions
+        self.stocks = stocks
+        self.cov_matrix = np.cov([stock.get_returns() for stock in stocks])
+        self.stddev = stddev(self.proportions, self.cov_matrix)
+        self.var = self.stddev**2
+        self.avg_return = proportions.dot(
+            [stock.get_annual_return() for stock in stocks])
+
+    def __lt__(self, other):
+        return self.var < other.var
+
+    def __gt__(self, other):
+        return self.var > other.var
 
 
 class sharpe_optimizer:
@@ -15,7 +130,7 @@ class sharpe_optimizer:
                   stddev(x, cov_matrix))
         return f
 
-    def __init__(self, stocks: List[stock.Stock], risk_free_rate, allow_short=False):
+    def __init__(self, stocks: List[Stock], risk_free_rate, allow_short=False):
         profits = [stock.get_returns() for stock in stocks]
         x = np.ones(len(profits))
         mean_vector = [stock.get_annual_return() for stock in stocks]
@@ -36,7 +151,7 @@ class sharpe_optimizer:
 
 
 class mvp_optimizer:
-    def __init__(self, stocks: List[stock.Stock]):
+    def __init__(self, stocks: List[Stock]):
         profits = [stock.get_returns() for stock in stocks]
         x = np.ones(len(profits))
         mean_vector = [stock.get_annual_return() for stock in stocks]
@@ -48,7 +163,7 @@ class mvp_optimizer:
                                         constraints=cons)
 
 
-def find_random_portfolio():
+def find_random_portfolio(_):
     all_tickers = [
         "AN",
         "BFRA",
@@ -106,7 +221,7 @@ def find_random_portfolio():
         n_period = None
         while len(random_stocks) < 10:
             try:
-                _stock = stock.Stock(
+                _stock = Stock(
                     all_tickers[int(len(all_tickers) * random())])
             except:
                 continue
@@ -121,16 +236,16 @@ def find_random_portfolio():
             if len(random_stocks) > 4:
                 _proportions = np.array(
                     [1/len(random_stocks)] * len(random_stocks))
-                random_portfolio = stock.Portfolio(_proportions, random_stocks)
+                random_portfolio = Portfolio(_proportions, random_stocks)
                 if random_portfolio.stddev < 0.05 and random_portfolio.avg_return > 0.1:
                     found = True
                     break
-    return {
+    return jsonify({
         "tickers": [s.ticker for s in random_portfolio.stocks],
         "proportions": random_portfolio.proportions.tolist(),
         "annual_return": random_portfolio.avg_return,
         "stddev": random_portfolio.stddev
-    }
+    })
 
 
 def main(ticker_a=None, ticker_b=None):
@@ -144,8 +259,8 @@ def main(ticker_a=None, ticker_b=None):
         ticker_b = input("Enter stock ticker B\n")
 
     try:
-        stock_A = stock.Stock(ticker_a)
-        stock_B = stock.Stock(ticker_b)
+        stock_A = Stock(ticker_a)
+        stock_B = Stock(ticker_b)
     except YahooFinanceError as e:
         return {
             "error": e.message
@@ -173,7 +288,7 @@ def main(ticker_a=None, ticker_b=None):
     logger.info("variance of {}: {}".format(stock_B.ticker, stock_B.get_var()))
 
     mvp = mvp_optimizer([stock_A, stock_B])
-    min_portfolio = stock.Portfolio(mvp.result.x, [stock_A, stock_B])
+    min_portfolio = Portfolio(mvp.result.x, [stock_A, stock_B])
 
     logger.info("")
     logger.info("MVP proportion {} {}".format(
@@ -200,10 +315,10 @@ def main(ticker_a=None, ticker_b=None):
     logger.info("Market portfolio standard deviation: {}%".format(
         sharpe.stddev * 100))
 
-    rf = stock.Stock.risk_free(0.02, stock_A)
-    market_portfolio = stock.Stock.combine(sharpe.result.x, [stock_A, stock_B])
-    case2 = stock.Portfolio(np.array([0.5, 0.5]), [rf, market_portfolio])
-    case3 = stock.Portfolio(np.array([-0.5, 1.5]), [rf, market_portfolio])
+    rf = Stock.risk_free(0.02, stock_A)
+    market_portfolio = Stock.combine(sharpe.result.x, [stock_A, stock_B])
+    case2 = Portfolio(np.array([0.5, 0.5]), [rf, market_portfolio])
+    case3 = Portfolio(np.array([-0.5, 1.5]), [rf, market_portfolio])
 
     logger.info("")
     logger.info("Case 2:")
